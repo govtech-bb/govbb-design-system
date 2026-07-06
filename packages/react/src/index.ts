cea3612c1f20fba1e1bb493e80485e5173dc448a
Aaron Harris <aaronah02@icloud.com>
2026-07-06 12:27:25 -0400

    feat: export all component wrappers from the react package

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,44 @@
-export { Button } from './button';
-export type { ButtonProps } from './button';
+export { Breadcrumbs } from './breadcrumbs/breadcrumbs';
+export type { BreadcrumbsProps } from './breadcrumbs/breadcrumbs';
+export { Button } from './button/button';
+export type { ButtonProps } from './button/button';
+export { Checkbox } from './checkbox/checkbox';
+export type { CheckboxProps } from './checkbox/checkbox';
+export { DateInput } from './date-input/date-input';
+export type { DateInputProps } from './date-input/date-input';
+export { ErrorSummary } from './error-summary/error-summary';
+export type { ErrorSummaryProps } from './error-summary/error-summary';
+export { FileUpload } from './file-upload/file-upload';
+export type { FileUploadProps } from './file-upload/file-upload';
+export { Footer, FooterLink } from './footer/footer';
+export type { FooterProps, FooterLinkProps } from './footer/footer';
+export { ErrorMessage, Fieldset, FormGroup, Hint, Label } from './form/form';
+export type {
+  ErrorMessageProps,
+  FieldsetProps,
+  FormGroupProps,
+  HintProps,
+  LabelProps,
+} from './form/form';
+export { Header } from './header/header';
+export type { HeaderProps } from './header/header';
+export { Input, Textarea } from './input/input';
+export type { InputProps, TextareaProps } from './input/input';
+export { Link } from './link/link';
+export type { LinkProps } from './link/link';
+export { List } from './list/list';
+export type { ListProps } from './list/list';
+export { NumberInput } from './number-input/number-input';
+export type { NumberInputProps } from './number-input/number-input';
+export { OfficialBanner } from './official-banner/official-banner';
+export type { OfficialBannerProps } from './official-banner/official-banner';
+export { Radio } from './radio/radio';
+export type { RadioProps } from './radio/radio';
+export { Search } from './search/search';
+export type { SearchProps } from './search/search';
+export { Select } from './select/select';
+export type { SelectProps } from './select/select';
+export { ShowHide } from './show-hide/show-hide';
+export type { ShowHideProps } from './show-hide/show-hide';
+export { StatusBanner } from './status-banner/status-banner';
+export type { StatusBannerProps } from './status-banner/status-banner';
